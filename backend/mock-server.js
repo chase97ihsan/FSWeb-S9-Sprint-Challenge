80bd834a6a11929f94d91bb47d848f730a3da121
İhsan Doğan <ihsannote3@gmail.com>
2023-07-11 23:58:04 +0300

    first

--- backend/mock-server.js
+++ backend/mock-server.js
@@ -1,27 +1,21 @@
-const { setupServer } = require('msw/node')
-const { rest } = require('msw')
+const { setupServer } = require("msw/node");
+const { rest } = require("msw");
 
-const { buildResponse } = require('./helpers')
+const { buildResponse } = require("./helpers");
 
 async function result(req, res, ctx) {
-  const [status, payload] = await buildResponse(req)
-  return res(
-    ctx.status(status),
-    ctx.json(payload),
-  )
+  const [status, payload] = await buildResponse(req);
+  return res(ctx.status(status), ctx.json(payload));
 }
 
 function catchAll(req, res, ctx) {
-  const message = `Endpoint [${req.method}] /${req.params['0']} does not exist`
-  return res(
-    ctx.status(404),
-    ctx.json({ message }),
-  )
+  const message = `Endpoint [${req.method}] /${req.params["0"]} does not exist`;
+  return res(ctx.status(404), ctx.json({ message }));
 }
 
 const handlers = [
-  rest.post('http://localhost:9000/api/result', result),
-  rest.all('http://localhost:9000/*', catchAll),
-]
+  rest.post("http://localhost:9000/api/result", result),
+  rest.all("http://localhost:9000/*", catchAll),
+];
 
-module.exports = setupServer(...handlers)
+module.exports = setupServer(...handlers);
--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,51 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from "react";
+import {
+  getByLabelText,
+  getByPlaceholderText,
+  render,
+  screen,
+  waitFor,
+  userEvent,
+} from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import AppFunctional from "./AppFunctional";
+import userEvent from "@testing-library/user-event";
+
+test("sanity", () => {
+  expect(true).toBe(true);
+});
+test("hata olmadan render ediliyor", async () => {
+  render(<AppFunctional />);
+});
+
+test("x ve y nin başlagıç kordinatları doğru gösteriliyor mu?", async () => {
+  render(<AppFunctional />);
+  const kareler = screen.getByText(/Koordinatlar/i);
+  expect(kareler).toHaveTextContent("(2,2)");
+});
+test("aşağı butonuna basınca kordinatlar doğru gösteriliyor mu?", async () => {
+  render(<AppFunctional />);
+  const down = screen.getByText(/Koordinatlar/i);
+  const downButton = screen.getByText("AŞAĞI");
+  userEvent.click(downButton);
+  expect(down).toHaveTextContent("(2,3)");
+});
+test("sınıra geldikten sonra kareyi yeniden aynı yönde hareket ettirince hata mesajı yansıtıyor mu?", async () => {
+  render(<AppFunctional />);
+
+  const downButton = screen.getByText("AŞAĞI");
+  userEvent.click(downButton);
+  userEvent.click(downButton);
+  const mesaj = screen.getByTestId("message");
+  expect(mesaj).toHaveTextContent("Aşağıya gidemezsiniz");
+});
+test("emaili hatalı girince datadan gelen mesaj ekrana yansıtılıyor mu?", async () => {
+  render(<AppFunctional />);
+  const mesaj = screen.getByTestId("message");
+  const mail = screen.getByTestId("email");
+  userEvent.type(mail, "igmail.com");
+  const button = screen.getByTestId("submit");
+  userEvent.click(button);
+  expect(mesaj).toHaveTextContent("");
+});
